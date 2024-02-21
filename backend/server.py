937ab89b6d60528906c3cab8bd36dcafd8e79368
Nimi Williams <williamson.nimi@gmail.com>
2024-02-21 22:02:54 +0100

    adding make_query api

--- backend/server.py
+++ backend/server.py
@@ -4,28 +4,43 @@ from langchain_community.llms import Replicate
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
 from flask import Flask
+from flask_cors import CORS
 import os
+from flask import jsonify
+from flask import request
+
 
 app = Flask(__name__)
+cors = CORS(app)
+
+
+@app.route('/make_query', methods=['POST', 'GET'])
+def make_query():
+    """This function gets the query from the frontend"""
+    query = request.get_json()
+    print(query)
+
+
+
 @app.route('/')
 def index():
-    #return call_llama()
-    return {'results':"trial"}
+    question = "Trial"
+    return jsonify({'results':call_llama(question)})
 
-"""def call_llama():
-    REPLICATE_API_TOKEN = getpass()
-    os.environ["REPLICATE_API_TOKEN"] = REPLICATE_API_TOKEN
-
+def call_llama(question):
+    """This function calls the Llama 13b API and sends a question  to it
+    output: Answer from the model
+    """
+    password = os.environ.get('REPLICATE_API_TOKEN')
 
     llama2_13b = "meta/llama-2-13b-chat:f4e2de70d66816a838a89eeeb621910adffb0dd0baba3976c96980970978018d"
-    llm = Replicate(
+    llm = Replicate(replicate_api_token=password,
         model=llama2_13b,
         model_kwargs={"temperature": 0.01, "top_p": 1, "max_new_tokens":500}
     )
-    question = "who wrote the book Innovator's dilemma?"
     answer = llm(question)
 
-    # chat history not passed so Llama doesn't have the context and doesn't know this is more about the book
+    """# chat history not passed so Llama doesn't have the context and doesn't know this is more about the book
     followup = "tell me more"
     followup_answer = llm(followup)
 
@@ -40,8 +55,8 @@ def index():
     # pass context (previous question and answer) along with the follow up "tell me more" to Llama who now knows more of what
     memory.save_context({"input": question},
                         {"output": answer})
-    followup_answer = conversation.predict(input=followup) """
-    #return ({"answer":"Trial"})
+    followup_answer = conversation.predict(input=followup)"""
+    return (answer)
 
 
 if __name__ == '__main__':
