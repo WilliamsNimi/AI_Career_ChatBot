c8c187ef640af317828de1c45ff2247b2508f44e
Nimi Williams <williamson.nimi@gmail.com>
2024-02-21 21:55:22 +0100

    Fetching data from React to flask

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,22 +11,22 @@ function App() {
   useEffect(() => {
       // Using fetch to fetch the api from 
       // flask server it will be redirected to proxy
-      fetch("/").then((res) =>
+      fetch("http://127.0.0.1:5000/").then((res) =>
           res.json().then((data) => {
               // Setting a data from api
-              setData(data);
-              console.log(data);
-          })
+              setData(data.results);
+              console.log(data.results);
+          }).catch((err)=>{console.log(err)})
       );
-  }, []);
+  }, []); 
   return (
     <div className="App">
       <header className="App-header">
           AI Career Chatbot
       </header>
       <div className="App-body">
-        <textarea rows="30" cols="100" id="prompt">Please enter your question here</textarea>
-        <button type="submit" className="ask-Button" >Ask</button>
+        <textarea defaultValue= "Please enter your question here" rows="30" cols="100" id="prompt"/>
+        <button type="submit" className="ask-Button" onClick={displayResponse}>Ask</button>
         <p>----------------------------------------------------------------------------------------------------</p>
         <div>
           <p className="AI_responses" id="AI_responses">{data.results}</p>
@@ -37,9 +37,17 @@ function App() {
   );
 }
 
-/*function displayResponse(){
-  
-  document.getElementById("AI_responses").innerText = ""
-} */
+function displayResponse(path, data){
+  console.log(JSON.stringify(document.getElementById('prompt').value))
+  return fetch('http://127.0.0.1:5000/make_query'),
+  {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify(document.getElementById('prompt').value)
+  }
+}
 
 export default App;
