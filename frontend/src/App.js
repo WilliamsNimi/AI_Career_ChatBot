c6e284cd7b9e9284a421b74c847531ed25b0ca0c
Williams Nimi <williamsnimi@Damilolas-MacBook-Air.local>
2025-04-02 21:14:46 +0100

    Transition

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-          <h1>Dami</h1>
-          <h5>AI Career Chatbot</h5>
+          <h1>Architecture Buddy</h1>
       </header>
       <div className="App-body">
         <div class="form-area">
-          <input type="text" id="fname" name="fname" defaultValue={"Enter your first Name"}/><br/><br/>
-          <input type="text" id="skills" name="skills" defaultValue={"Type your skillsets here"}/><br/><br/>
           <textarea defaultValue= "Please enter your question here" rows="30" cols="100" id="prompt"/>
           <button type="submit" className="ask-Button" onClick={()=>displayResponse(setData)}>Ask</button>
           <p id="loading"></p>
@@ -27,28 +24,25 @@ function App() {
 
   );
 }
-
 function displayResponse(setData){
   // This function sends a post request to the server with the text area value as payload.
-  var firstname = document.getElementById("fname").value
-  var skills = document.getElementById("skills").value
   document.getElementById("loading").innerHTML = "loading..."
-  return fetch('http://127.0.0.1:5000/query',
+  return fetch('https://sdg0vxa0k4.execute-api.us-east-1.amazonaws.com/Prod',
   {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: JSON.stringify({"query": document.getElementById('prompt').value, "firstname":firstname, "skills":skills})
+    body: JSON.stringify({"query": document.getElementById('prompt').value})
   }).then((response)=> response.json())
   .then((data)=>{
-    setData(data.results);
+    const parsedData = JSON.parse(data.body);
+    setData(parsedData.response);
     document.getElementById("loading").innerHTML = ""
-    console.log(data);
+    console.log(data.body);
   })
   .catch((err)=>{
     console.log(err.message);
   });
 }
-
 export default App;
